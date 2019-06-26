2aeee4648bfb2120696765312a27d6607279b4a9
粟野未早希 <awanomisaki@awanomisakinoMacBook-Air.local>
2019-06-26 14:13:45 +0900

    ユーザー削除・追加機能導入

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,6 +1,7 @@
 $(function() {
   
   var search_list = $("#user-search-result");
+  var member_list = $("#member_search_result");
 
   function appendUser(user) {
       var html = `<div class="chat-group-user clearfix">
@@ -17,6 +18,14 @@ $(function() {
     search_list.append(html);
   }
 
+  function appendGroup(name, user_id) {
+    var html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
+                <input name='group[user_ids][]' type='hidden' value=${ user_id }>
+                <p class='chat-group-user__name'>${ name }</p>
+                <div class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</div>
+                </div>`
+    member_list.append(html);
+  }
 
   $("#user-search-field").on("keyup", function() {
     var input = $("#user-search-field").val();
@@ -39,5 +48,16 @@ $(function() {
         appendErrMsgToHTML("一致するユーザーはいません");
       }
     })
+  });
+  $(function(){
+    $("#user-search-result").on('click', 'a', function(){
+      var user_id = $(this).data('user-id');
+      var name = $(this).data('user-name');
+      $(this).parent().remove();
+      appendGroup(name, user_id);
+    });
+    $("#member_search_result").on('click', '.user-search-remove', function(){
+      $(this).parent().remove();
+    });
   });
 });
